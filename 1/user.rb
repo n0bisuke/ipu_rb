4da17328dd79c9f698818d03bf983beb91cd6324
KOMATSU Issei <issei@mizuhataya.com>
2013-11-04 17:00:27 +0900

    User クラスを実装してテストした

--- 1/user.rb
+++ 1/user.rb
@@ -1,2 +1,15 @@
 class User
+  attr_reader :name
+  attr_accessor :height, :weight
+  
+  def initialize name, height, weight
+    @name = name
+    @height = height
+    @weight = weight
+  end
+  
+  
+  def bmi
+    bmi = @weight / ((@height / 100.0) ** 2)
+  end
 end
--- 1/user_test.rb
+++ 1/user_test.rb
@@ -1,6 +1,32 @@
 require 'test/unit' 
+require './user.rb'
 
 class Test_Hoge < Test::Unit::TestCase
-  def test_aaa
+  def setup
+    @user = User.new "Taro", 180, 60
   end
+  
+  def test_bmi
+    u = User.new "Taro", 170, 60
+    assert u.bmi().round(1) == 20.8, "BMI is about 20.8"
+  end
+#  
+#  def test_read_height
+#    assert @user.height == 180, "Read Height"
+#  end
+#  
+#  def test_write_height
+#    @user.height = 170
+#    assert @user.height == 170, "Write height"
+#  end
+#  
+#  def test_read_weight
+#    assert @user.weight == 60, "Read Weight"
+#  end
+#  
+#  def test_write_weight
+#    @user.weight = 70
+#    assert @user.weight == 70, "Write Weight"
+#  end
+#  
 end
