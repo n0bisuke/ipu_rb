ed4d13e3558422c8086246e5d222fe0825d539d0
whtiech0c0 <hirotonbuu@gmail.com>
2013-11-04 18:13:30 +0900

    「韓国語のテスト」に修正

--- 1/korean_test.rb
+++ 1/korean_test.rb
@@ -9,6 +9,6 @@ class Test_Korean < Test::Unit::TestCase
   
   def test_tweet_kr
     text = @user.make_text_kr(10)
-    assert text == "내일의 최저기온은 10도입니다.", "text"
+    assert text == "내일의 최저기온은 10도입니다.", "韓国語のテスト"
   end
 end
