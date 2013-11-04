0fa52f15c28056a6121425d8a8f4166470a00905
zaku <zaku@example.com>
2013-11-04 09:26:11 +0000

    returnけしさったった

--- 1/french.rb
+++ 1/french.rb
@@ -2,6 +2,6 @@ require './user.rb'
 
 class French < User
 	def make_text min_temp
-	return "Temperature minimale pour demain est de #{min_temp} degres"
+	"Temperature minimale pour demain est de #{min_temp} degres"
 	end
 end 
